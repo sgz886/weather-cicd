68971040cf11f5a426a99b0b930ceb9fed05cbcb
Gordon <sgz886@gmail.com>
2023-04-05 11:46:25 +1000

    feat: add support to endpoint "/"

--- src/main/java/com/example/weather/controller/CurrentWeatherController.java
+++ src/main/java/com/example/weather/controller/CurrentWeatherController.java
@@ -6,11 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class CurrentWeatherController {
 
     private final WeatherService weatherService;
+
+    @GetMapping("/")
+    public RedirectView login(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", "use location: Melbourne");
+        return new RedirectView("/current-weather");
+    }
 
     @GetMapping(value = {"/current-weather","/current-weather/{location}"})
     public String getCurrentWeather(@PathVariable Optional<String> location,Model model) {
